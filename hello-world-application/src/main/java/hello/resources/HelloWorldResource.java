b41b4acde32d3032ea9dce068bab6cffa41ef445
daniel <daniel.eriksson@axiomatics.com>
2018-04-13 16:12:46 +0200

    Added adder end point.

--- hello-world-application/src/main/java/hello/resources/HelloWorldResource.java
+++ hello-world-application/src/main/java/hello/resources/HelloWorldResource.java
@@ -1,7 +1,11 @@
 package hello.resources;
 
+import adder.Adder;
 import com.codahale.metrics.annotation.Timed;
+import hello.AppConfiguration;
 import hello.GreeterService;
+import hello.api.Added;
+import java.lang.invoke.MethodHandles;
 import java.util.Optional;
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -12,22 +16,26 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import hello.api.Saying;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/hello-world")
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
 
-    private final String template;
-    private final String defaultName;
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    private final AppConfiguration configuration;
     private final GreeterService greeterService;
+    private final Adder adder;
     private final Provider<HttpServletRequest> requestProvider;
 
 
     @Inject
-    public HelloWorldResource(String template, String defaultName, GreeterService greeterService, Provider<HttpServletRequest> requestProvider) {
-        this.template = template;
-        this.defaultName = defaultName;
+    public HelloWorldResource(AppConfiguration configuration, GreeterService greeterService, Adder adder, Provider<HttpServletRequest> requestProvider) {
+        this.configuration = configuration;
         this.greeterService = greeterService;
+        this.adder = adder;
         this.requestProvider = requestProvider;
     }
 
@@ -35,8 +43,15 @@ public class HelloWorldResource {
     @Timed
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
-        System.out.println("Context path: " + requestProvider.get().getContextPath());
+        LOG.info("Path info: " + requestProvider.get().getPathInfo());
+        return greeterService.sayHello(configuration.getTemplate(), name.orElse(configuration.getDefaultName()));
+    }
 
-        return greeterService.sayHello(template, name.orElse(defaultName));
+    @GET
+    @Path("/add")
+    @Timed
+    public Added add(@QueryParam("a") String a, @QueryParam("b") String b) {
+        int result = adder.add(Integer.parseInt(a), Integer.parseInt(b));
+        return new Added(result);
     }
 }
