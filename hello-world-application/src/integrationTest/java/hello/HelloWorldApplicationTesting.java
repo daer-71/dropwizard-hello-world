0cedf372b32fd267082ec16bb5a68a62f05ecc92
daniel <daniel.eriksson@axiomatics.com>
2018-04-13 16:10:06 +0200

    Added testing Guice module, cleaned up.

--- hello-world-application/src/integrationTest/java/hello/HelloWorldApplicationTesting.java
+++ hello-world-application/src/integrationTest/java/hello/HelloWorldApplicationTesting.java
@@ -1,31 +1,37 @@
 package hello;
 
+import hello.modules.AutoscanModule;
+import hello.modules.HelloWorldModule;
+import hello.modules.HelloWorldTestModule;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import hello.health.TemplateHealthCheck;
-import hello.resources.HelloWorldResource;
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.vyarus.dropwizard.guice.GuiceBundle;
 
 public class HelloWorldApplicationTesting extends Application<AppConfiguration> {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Override
     public String getName() {
-        return "hello-application hello";
+        return "hello-application-testing";
     }
 
     @Override
     public void initialize(Bootstrap<AppConfiguration> bootstrap) {
-        System.out.println("Initializeing...");
-        // nothing to do yet
+        LOG.info("Initializing testing app...");
+        bootstrap.addBundle(GuiceBundle.builder()
+            .enableAutoConfig(getClass().getPackage().getName())
+            .useWebInstallers()
+            .modules(new AutoscanModule(), new HelloWorldModule(), new HelloWorldTestModule())
+            .build());
     }
 
     @Override
     public void run(AppConfiguration configuration, Environment environment) {
-        final HelloWorldResource resource = new HelloWorldResource(configuration.getTemplate(), configuration.getDefaultName());
-        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
-        environment.healthChecks().register("template", healthCheck);
-        environment.jersey().register(resource);
-        System.out.println("Running...");
+        LOG.info("Running testing app...");
     }
 }
